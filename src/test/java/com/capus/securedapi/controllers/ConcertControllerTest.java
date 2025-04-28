07310bbdb0f3a3d2fad69ca92858d5ffcb1f44d3
Crapo01 <capusluc@gmail.com>
2025-04-28 18:23:36 +0200

    environement variables updated

---
+++ src/test/java/com/capus/securedapi/controllers/ConcertControllerTest.java
@@ -0,0 +1,101 @@
+package com.capus.securedapi.controllers;
+
+import com.capus.securedapi.entity.Concert;
+import com.capus.securedapi.repository.ConcertRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ConcertControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ConcertRepository concertRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        concertRepository.deleteAll();
+        concertRepository.saveAll(List.of(
+                new Concert(null,"first","","","","","","",""),
+                new Concert(null,"second","","","","","","","")
+        ));
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void shouldReturnAllConcerts() throws Exception {
+        mockMvc.perform(get("/api/concerts/all"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    @WithMockUser(roles = "EDITOR")
+    void shouldCreateConcert() throws Exception {
+        Concert concert = new Concert();
+
+        mockMvc.perform(post("/api/concerts/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(concert)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("Concert created"));
+    }
+
+    @Test
+    @WithMockUser(roles = "VIEWER")
+    void shouldNotCreateConcertBecauseUnauthorizedRole() throws Exception {
+        Concert concert = new Concert();
+
+        mockMvc.perform(post("/api/concerts/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(concert)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(roles = "EDITOR")
+    void shouldUpdateConcert() throws Exception {
+        Concert savedConcert = concertRepository.save(new Concert(null,"old","","","","","","",""));
+        Concert updatedConcert = concertRepository.save(new Concert(null,"new","","","","","","",""));
+
+        mockMvc.perform(put("/api/concerts/update/" + savedConcert.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedConcert)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("Concert updated"))
+                .andExpect(jsonPath("$.data.nom").value("new"));
+    }
+
+    @Test
+    @WithMockUser(roles = "EDITOR")
+    void shouldDeleteInformation() throws Exception {
+        Concert savedConcert = concertRepository.save(new Concert(null,"RIP","","","","","","",""));
+
+        mockMvc.perform(delete("/api/concerts/"+savedConcert.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Concert deleted"));
+    }
+
+
+}
--- src/test/java/com/capus/securedapi/controllers/InformationControllerTest.java
+++ src/test/java/com/capus/securedapi/controllers/InformationControllerTest.java
@@ -1,5 +1,9 @@
 package com.capus.securedapi.controllers;
 
+import com.capus.securedapi.entity.Information;
+import com.capus.securedapi.repository.InformationRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -13,6 +17,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,62 +36,76 @@ public class InformationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @WithMockUser(username = "user1", roles = "EDITOR")
-    @Test
-    public void testCreateInfo() throws Exception {
-        // Arrange
-        String infoJson = "{\"message\":\"Test unit test\"}";
+    @Autowired
+    private InformationRepository informationRepository;
 
-        // Act
-        ResultActions result = mockMvc.perform(post("/api/informations/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(infoJson));
+    @Autowired
+    private ObjectMapper objectMapper;
 
-        // Assert
-        result.andExpect(
-                status().isCreated())
-                .andExpect(content().contentType(MediaType. APPLICATION_JSON))
-                .andExpect(jsonPath("$.message").value("Information created"))
-                .andExpect(authenticated());
+    @BeforeEach
+    void setup() {
+        informationRepository.deleteAll();
+        informationRepository.saveAll(List.of(
+                new Information(null, "Info 1", false),
+                new Information(null, "Info 2", true)
+        ));
     }
 
     @Test
-    public void testUpdateInfo_HappyPath() throws Exception {
-        // Arrange
-        long userId = 40L;
-        String infoJson = "{\"message\":\"Test unit test\"}";
-
-        // Act
-        ResultActions result = mockMvc.perform(put("/api/informations/update/{id}", userId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(infoJson));
-
-        // Assert
-        result.andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType. APPLICATION_JSON))
-                .andExpect(jsonPath("$.message").value("Information updated"));
+    @WithMockUser(roles = "USER")
+    void shouldReturnAllInformation() throws Exception {
+        mockMvc.perform(get("/api/informations/all"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
     }
 
     @Test
-    public void testDeleteInfo_HappyPath() throws Exception {
-        // Arrange
-        long Id = 40L;
+    @WithMockUser(roles = "EDITOR")
+    void shouldCreateInformation() throws Exception {
+        Information info = new Information(null, "New Info", true);
 
-        // Act
-        ResultActions result = mockMvc.perform(delete("/api/informations/{id}", Id));
-
-        // Assert
-        result.andExpect(status().isOk());
+        mockMvc.perform(post("/api/informations/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(info)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("Information created"));
     }
+
     @Test
-    public void testDeleteInfo_NoInfoFound() throws Exception {
-        // Arrange
-        long Id = 0L;
+    @WithMockUser(roles = "VIEWER")
+    void shouldNotCreateInformationBecauseUnauthorizedRole() throws Exception {
+        Information info = new Information(null, "New Info", true);
 
-        // Act
-        ResultActions result = mockMvc.perform(delete("/api/informations/{id}", Id));
-
-        // Assert
-        result.andExpect(status().isNotFound());
+        mockMvc.perform(post("/api/informations/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(info)))
+                .andExpect(status().isForbidden());
     }
+
+    @Test
+    @WithMockUser(roles = "EDITOR")
+    void shouldUpdateInformation() throws Exception {
+        Information savedInfo = informationRepository.save(new Information(null, "Old Title", true));
+        Information updatedInfo = new Information(null, "Updated Title", false);
+
+        mockMvc.perform(put("/api/informations/update/" + savedInfo.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedInfo)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("Information updated"))
+                .andExpect(jsonPath("$.data.important").value(false))
+                .andExpect(jsonPath("$.data.message").value("Updated Title"));
+    }
+
+    @Test
+    @WithMockUser(roles = "EDITOR")
+    void shouldDeleteInformation() throws Exception {
+        Information savedInfo = informationRepository.save(new Information(null, "RIP", true));
+        List<Information> infos = informationRepository.findAll();
+        mockMvc.perform(delete("/api/informations/"+savedInfo.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Information deleted"));
+    }
+
+
 }
