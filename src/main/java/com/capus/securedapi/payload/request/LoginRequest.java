4d45fae2fc5dec5b77594654ee1d6213ece4fb45
Crapo01 <capusluc@gmail.com>
2025-06-16 11:06:48 +0200

    swagger doc updated for DWWM demo

--- src/main/java/com/capus/securedapi/payload/request/LoginRequest.java
+++ src/main/java/com/capus/securedapi/payload/request/LoginRequest.java
@@ -1,12 +1,15 @@
 package com.capus.securedapi.payload.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
 	@NotBlank
+	@Schema( description = "String between 3 and 20 char.", example = "editor")
   private String username;
 
 	@NotBlank
+	@Schema(description = "complex password", example = "tIFQ4?&eh")
 	private String password;
 
 	public String getUsername() {
